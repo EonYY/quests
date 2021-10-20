e2eecf863d03c5b6a04a86faee4e88d63867893d
EonYY <eonblues@outlook.com>
2021-10-20 16:58:52 +0300

    Create first job form input field

--- first-quest/components/Job/NewJobModal.js
+++ first-quest/components/Job/NewJobModal.js
@@ -1,16 +1,48 @@
-import { Box, Grid, FilledInput, Select, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions } from "@mui/core"
+import { TextField, FormControl, Box, Grid, FilledInput, Select, MenuItem, Dialog, DialogTitle, DialogContent, DialogActions, InputLabel } from "@mui/material"
 
 
 
 export default function NewJobModal() {
     return (
-        <Dialog>
+        <Dialog open={true} fullWidth>
             <DialogTitle>New Job</DialogTitle>
             <DialogContent>
-                <Grid container>
+                <Grid container spacing={2}>
+
                     <Grid item xs={6}>
-                        <FilledInput placeholder="Job Title" />
+                        <TextField
+                            autoFocus
+                            disableUnderline
+                            required
+                            autoComplete="off"
+                            margin="dense"
+                            label="Job Title"
+                            fullWidth
+                            variant="filled"
+                            size="small"
+                            placeholder="Ex: 3D Character Artist"
+                        />
                     </Grid>
+
+                    <Grid item xs={6}>
+                        <TextField
+                            autoFocus
+                            disableUnderline
+                            required
+                            select
+                            margin="dense"
+                            label="Job Category"
+                            fullWidth
+                            variant="filled"
+                            size="small"
+                        >
+                            <MenuItem value="Game Development">Game Development</MenuItem>
+                            <MenuItem value="eSports">eSports</MenuItem>
+                            <MenuItem value="Customer Service">Customer Service</MenuItem>
+                            <MenuItem value="Business">Business</MenuItem>
+                        </TextField>
+                    </Grid>
+                    
                 </Grid>
             </DialogContent>
         </Dialog>
